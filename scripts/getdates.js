a02bf789c7fddf5c59782d9e68b82380a4e4ea3c
Egor Smirnov <exsplorator@gmail.com>
2023-09-16 15:46:13 -0600

    i hate it

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,4 +2,4 @@ let year = document.querySelector("#year");
 year.innerHTML = "&copy; " + new Date().getFullYear();
 
 let lastModifiedParagraph = document.getElementById("lastModified");
-lastModifiedParagraph.textContent = "Last Modified" + document.lastModified;
+lastModifiedParagraph.textContent = "Last Modified " + document.lastModified;
