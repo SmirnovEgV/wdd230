e835fd7e2a71fd6306256120b14a950648126ea5
Egor Smirnov <exsplorator@gmail.com>
2023-10-19 14:38:17 -0600

    I hate it

--- scripts/burgmenu.js
+++ scripts/burgmenu.js
@@ -1,9 +1,11 @@
-burger = document.querySelector('.hamburger-menu-button')
+burger = document.querySelector('#burger')
 navmenu = document.querySelector('#nav-menu')
+temple_txt = document.querySelector('#temple-title')
 
 if (burger) {
     burger.addEventListener('click', function () {
         burger.classList.toggle('active')
         navmenu.classList.toggle('active')
+        temple_txt.style.top = '55%'
     })
 }
