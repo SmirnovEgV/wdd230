5e0bd44d39f0e9926e666c87eb68ee32292bc9c3
Egor Smirnov <exsplorator@gmail.com>
2023-11-09 15:51:42 -0700

    button change added

--- chamber/scripts/darkmode.js
+++ chamber/scripts/darkmode.js
@@ -2,5 +2,6 @@ var button = document.querySelector("#dark-mode")
 var body = document.querySelector("body")
 
 button.addEventListener("click", () => {
+    button.classList.toggle("dark-mode1")
     body.classList.toggle("dark-mode")
 })
