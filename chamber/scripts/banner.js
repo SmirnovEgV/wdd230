fce07f794c1c2903b849173e6fcc5c37e112d474
Egor Smirnov <exsplorator@gmail.com>
2023-11-18 20:56:33 -0700

    banner and things

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,12 +1,14 @@
 let day = new Date();
 let weekday = day.getDay();
 
-if (weekday == 0 || weekday == 1 || weekday == 2 || weekday == 4){
+if (weekday == 1 || weekday == 2 || weekday == 3 ){
     let var1 = document.getElementById("banner-meet")
-    var1.style.display = "block";
+    var1.style.display = "flex";
 }
 
 let x = document.getElementById("close-btn")
 x.addEventListener('click', ()=>{
+    let var1 = document.getElementById("banner-meet")
+    var1.style.display = "none";
 
 })
