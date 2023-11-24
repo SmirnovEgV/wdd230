39b4dc43b6bc22633b8fdb179dd1aab7e9258b7f
Egor Smirnov <exsplorator@gmail.com>
2023-11-24 14:04:23 -0700

    Buthurht

--- chamber/scripts/burgmenu.js
+++ chamber/scripts/burgmenu.js
@@ -1,13 +1,20 @@
 burger = document.querySelector('.hamburger-menu-button')
 navmenu = document.querySelector('#nav-menu')
 
+burger.addEventListener('click', function () {
+    if (burger.innerHTML != '☰'){
+        burger.innerHTML = '☰'
+    }
+    else {
+        burger.innerHTML = 'X'
+    }
+})
+
 if (burger) {
     burger.addEventListener('click', function () {
         navmenu.classList.toggle('active')
     })
 }
-
-
 
 document.getElementById("calltoaction").addEventListener("click", function() {
     window.location.href = "join.html"; // Replace "join.html" with the actual URL of the page you want to link to
