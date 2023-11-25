e9cf280d3469e9467cba5f6638b46a8e113131ce
Egor Smirnov <exsplorator@gmail.com>
2023-11-24 17:36:26 -0700

    updates n01

---
+++ chamber/scripts/joinbtn.js
@@ -0,0 +1,3 @@
+document.getElementById("calltoaction").addEventListener("click", function() {
+    window.location.href = "join.html"; // Replace "join.html" with the actual URL of the page you want to link to
+  });
