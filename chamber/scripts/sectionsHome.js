9e0a61ccfa3e968dee438e091b6da002f76f7c83
Egor Smirnov <exsplorator@gmail.com>
2023-11-24 18:20:49 -0700

    filtering

--- chamber/scripts/sectionsHome.js
+++ chamber/scripts/sectionsHome.js
@@ -4,23 +4,34 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch('./data/members.json')
         .then(response => response.json())
         .then(data => {
-            const indexes = new Set()
-            console.log("indexes", indexes)
-            while(indexes.size < 3){
-                indexes.add(Math.floor(Math.random() * 8 ))
+            // Filter members by Gold and Silver membership levels
+            const filteredMembers = data.members.filter(member => member.membershipLevel === "Gold" || member.membershipLevel === "Silver");
+
+            // Ensure there are at least 3 members with Gold or Silver membership
+            if (filteredMembers.length >= 3) {
+                // Randomly select 3 members
+                const indexes = [];
+                while (indexes.length < 3) {
+                    const randomIndex = Math.floor(Math.random() * filteredMembers.length);
+                    if (!indexes.includes(randomIndex)) {
+                        indexes.push(randomIndex);
+                    }
+                }
+
+                // Display the selected members in the spotlight section
+                const spotlightsSection = document.getElementById('spotlights');
+                indexes.forEach(i => {
+                    let newsection = document.createElement("section");
+                    newsection.innerHTML = `<h3>${filteredMembers[i].name}</h3> <img src=${filteredMembers[i].imageURL} alt=${filteredMembers[i].name} Logo><p>${filteredMembers[i].text}</p>`;
+                    spotlightsSection.append(newsection);
+                });
+            } else {
+                console.error('Not enough members with Gold or Silver membership for spotlight.');
             }
-            console.log("ss", indexes)
-            const spotlightsSection = document.getElementById('spotlights');
-            var j = 0
-            indexes.forEach(i => {
-                let newsection = document.createElement("section");
-                newsection.innerHTML = `<h3>${ data.members[i].name}</h3> <img src=${data.members[i].imageURL} alt=${data.members[i].name} Logo><p>${data.members[i].text}</p>`
-                spotlightsSection.append(newsection)
-                j++
-            })
         })
         .catch(error => console.error('Error fetching JSON:', error));
 });
+
 
 // Function to shuffle an array
 function shuffleArray(array) {
