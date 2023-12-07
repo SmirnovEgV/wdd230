4ad72fca661b177f095977d589c57f071d8889c2
Egor Smirnov <exsplorator@gmail.com>
2023-12-06 17:16:48 -0700

    Nav and footer small view

--- bountifulFoods/scripts/common.js
+++ bountifulFoods/scripts/common.js
@@ -1,5 +1,23 @@
 let year = document.querySelector("#year");
-year.innerHTML = "&copy; " + new Date().getFullYear();
+year.innerHTML = "No rights reserved &copy; " + new Date().getFullYear();
 
 let lastModifiedParagraph = document.getElementById("lastModified");
 lastModifiedParagraph.textContent = "Last Modified " + document.lastModified;
+
+burger = document.querySelector('.hamburger-menu-button')
+navmenu = document.querySelector('#nav-menu')
+
+burger.addEventListener('click', function () {
+    if (burger.innerHTML != '☰'){
+        burger.innerHTML = '☰'
+    }
+    else {
+        burger.innerHTML = 'X'
+    }
+})
+
+if (burger) {
+    burger.addEventListener('click', function () {
+        navmenu.classList.toggle('active')
+    })
+}
